eb4044adc38fe2b6ffa867ab5b83163e62637114
Jordan West <jordwest@gmail.com>
2020-01-18 14:23:15 +1100

    traverses root to find git projects

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod commands;
 mod config;
+mod traverse;
+
+use config::Config;
 
 fn main() {
     println!("Hello, world!");
 
-    // let config = Config::autoload();
+    let config = Config::autoload().unwrap();
     // println!("{:?}", config);
 
-    commands::configure::configure().unwrap();
+    // commands::configure::configure().unwrap();
+    let root = traverse::Root::traverse(&config).unwrap();
+    println!("{:#?}", root);
 }
