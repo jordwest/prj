4152b932f3d9cee83e4980d9416d78680103035d
Jordan West <jordwest@gmail.com>
2020-01-20 16:26:33 +1100

    Better git status reporting

--- src/discovery/git.rs
+++ src/discovery/git.rs
@@ -1,5 +1,6 @@
 use super::cache::VcsInfo;
-use git2::Repository;
+use git2::StatusEntry;
+use git2::{Repository, StatusOptions};
 use std::path::Path;
 
 #[derive(Debug)]
@@ -15,6 +16,14 @@ pub fn fetch_vcs_info(path: &Path) -> Result<VcsInfo, GitError> {
     let repo = Repository::open(path).or(Err(FailedToOpen))?;
     let head = repo.head().or(Err(FailedToReadHead))?;
 
+    let statuses = repo
+        .statuses(Some(
+            StatusOptions::new()
+                .include_ignored(false)
+                .include_untracked(true),
+        ))
+        .or(Err(FailedToReadStatus))?;
+
     let vcs_info = VcsInfo {
         last_commit_summary: head
             .peel_to_commit()
@@ -25,7 +34,7 @@ pub fn fetch_vcs_info(path: &Path) -> Result<VcsInfo, GitError> {
 
         current_branch_name: head.shorthand().ok_or(FailedToReadHead)?.to_string(),
 
-        uncommitted_changes: repo.statuses(None).or(Err(FailedToReadStatus))?.len(),
+        uncommitted_changes: statuses.len(),
     };
     Ok(vcs_info)
 }
